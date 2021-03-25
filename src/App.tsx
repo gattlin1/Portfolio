0241bfdd214d89b49eba8ea5d96d02b2b6f45314
gattlin1 <gattlin1@live.missouristate.edu>
2021-03-24 22:31:16 -0400

    started header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header/Header';
+import './App.scss';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <header>
+      <div id='title'>Gattlin Walker</div>
+      <div>Page 1</div>
+      <div>Page 2</div>
+      <div>Resume</div>
+    </header>
+  );
+}
+
+export default Header;
