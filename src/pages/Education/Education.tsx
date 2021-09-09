524cb51a836bc905219c04efa38b3cb8fa526ac6
gattlin1 <gattlin1@live.missouristate.edu>
2021-09-09 14:32:07 -0400

    Added MSU name logo. Started working on education page.

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect } from 'react';
+import MSUNameLogo from '../../components/Logo/MSUNameLogo/MSUNameLogo';
+
+import './Education.scss';
 
 function Education() {
   useEffect(() => {
     document.title = 'Gattlin Walker | Education';
   }, []);
-  return <div className='education'></div>;
+  return (
+    <div className='education'>
+      <div className='school-logo-container'>
+        <MSUNameLogo />
+      </div>
+    </div>
+  );
 }
 
 export default Education;
